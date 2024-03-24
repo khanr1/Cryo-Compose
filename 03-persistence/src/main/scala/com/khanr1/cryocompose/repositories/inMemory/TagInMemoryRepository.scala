1d61f04db042198793824d2b2398a76606ba8991
khanr1 <raphael.khan@gmail.com>
2024-03-24 20:39:44 +0200

    [Tag] Tag InMemoryRepo working: The test done so far are passing

--- 03-persistence/src/main/scala/com/khanr1/cryocompose/repositories/inMemory/TagInMemoryRepository.scala
+++ 03-persistence/src/main/scala/com/khanr1/cryocompose/repositories/inMemory/TagInMemoryRepository.scala
@@ -3,8 +3,9 @@ package cryocompose
 package repositories
 package inMemory
 
-import cats.effect.*
+import cats.effect.Ref
 import cats.*
+import cats.implicits.*
 
 /** The TagInMemoryRepository object provides an implementation
   * of the TagRepository trait using an in-memory storage mechanism.
@@ -13,7 +14,7 @@ import cats.*
   */
 
 object TagInMemoryRepository:
-  def make[F[_]: Functor: FlatMap](state: Ref[F, Vector[Tag[Int]]]): TagRepository[F, Int] =
+  def make[F[_]: MonadThrow](state: Ref[F, Vector[Tag[Int]]]): TagRepository[F, Int] =
     type TagID = Int
     new TagRepository[F, TagID]:
       private val nextInt: F[Int] = state.get.map(_.size)
@@ -23,6 +24,18 @@ object TagInMemoryRepository:
           .map(Tag(_, name))
           .flatMap(tag => state.modify(s => (s :+ tag) -> tag))
 
-      override def delete(id: TagID): F[Unit] = state.update(x => x.filterNot(_.id === id))
+      override def delete(id: TagID): F[Unit] =
+        state.get.flatMap { tags =>
+          if tags.exists(tag => tag.id === id) then state.update(x => x.filterNot(_.id === id))
+          else
+            MonadThrow[F]
+              .raiseError(
+                throw new java.lang.RuntimeException(
+                  s"Failed to delete todo: ${id} because it didn't exist."
+                )
+              )
+
+        }
+        // state.update(x => x.filterNot(_.id === id))
 
       override def findAll(): F[Vector[Tag[TagID]]] = state.get
