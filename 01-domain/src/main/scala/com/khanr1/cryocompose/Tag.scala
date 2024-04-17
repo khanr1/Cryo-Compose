d91097a8251116d03624473f0ac369d42c2b1981
khanr1 <raphael.khan@gmail.com>
2024-04-17 21:21:38 +0300

    [Cleaning] Tag encoder written

--- 01-domain/src/main/scala/com/khanr1/cryocompose/Tag.scala
+++ 01-domain/src/main/scala/com/khanr1/cryocompose/Tag.scala
@@ -1,6 +1,12 @@
 package com.khanr1
 package cryocompose
 
+import cats.Eq
+import cats.Show
+import io.circe.Decoder
+import io.circe.Encoder
+import io.github.iltotore.iron.circe.given
+
 import io.github.iltotore.iron.*
 import io.github.iltotore.iron.constraint.all.*
 
@@ -12,6 +18,12 @@ import io.github.iltotore.iron.constraint.all.*
   *   is the name of the Tag
   */
 final case class Tag[TagID](id: TagID, name: TagName)
+
+object Tag:
+  given show[TagID]: Show[Tag[TagID]] = Show.fromToString
+  given eq[TagID]: Eq[Tag[TagID]] = Eq.fromUniversalEquals
+  given encoder[TagID: Encoder]: Encoder[Tag[TagID]] =
+    Encoder.forProduct2("id", "name")(t => (t.id, t.name))
 
 type TagNameR = DescribedAs[Not[Empty], "The name of a tag cannot be empty"]
 opaque type TagName = String :| TagNameR
